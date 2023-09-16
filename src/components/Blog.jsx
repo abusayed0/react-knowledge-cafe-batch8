eb81e33b36d033d5e1f80ec1ee5b33089298e491
abusayed0 <abusayed48876@gmail.com>
2023-09-16 15:00:56 +0600

    added blog content

---
+++ src/components/Blog.jsx
@@ -0,0 +1,34 @@
+import PropTypes from 'prop-types';
+
+const Blog = ({ blog }) => {
+    const { title, cover, author_img, author, posted_date, reading_time, hashtags} = blog;
+    return (
+        <article>
+            {/* cover  */}
+            <img className="w-full h-[450px]" src={cover} alt={`Cover of ${title}`} />
+            <div className="mt-8 mb-4 flex justify-between items-center">
+                <div className="flex gap-6 items-center">
+                    <img className="h-14 w-14 rounded-full" src={author_img} alt="" />
+                    <div>
+                        <p className="text-2xl font-bold">{author}</p>
+                        <p className="font-semibold">{posted_date}</p>
+                    </div>
+                </div>
+                <div>
+                    <p className="font-medium text-xl">{`${reading_time < 10 ? "0" : ""}${reading_time} min read`}</p>
+                </div>
+            </div>
+            <h3 className="text-3xl font-bold">{title}</h3>
+            <div className="mt-4 mb-7 flex gap-4 font-medium text-xl">
+                {
+                    hashtags.map((tag, index) => <a  key={index}>#{tag}</a>)
+                }
+            </div>
+            <button className="mb-8 text-[#6047EC] text-xl font-semibold underline">Mark as read</button>
+        </article>
+    );
+};
+Blog.propTypes = {
+    blog: PropTypes.object.isRequired
+}
+export default Blog;
--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { useState } from "react";
+import Blog from "./Blog";
 
 const Blogs = () => {
     const [blogs, setBlogs] = useState([]);
@@ -7,12 +8,19 @@ const Blogs = () => {
         const url = "data/blog.json";
         fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => setBlogs(data))
     },[]);
     return (
         <div className="lg:col-span-2 border">
-            <h3 className="text-2xl md-text-3xl font-bold">Blogs - {blogs.length}</h3>
-            
+            <h3 className="text-3xl md-text-4xl font-bold">Blogs - {blogs.length}</h3>
+            <div className="mt-7 grid grid-cols-1 gap-10">
+                {
+                    blogs.map(blog => <Blog
+                    key={blog.id}
+                    blog={blog}
+                    />)
+                }
+            </div>
         </div>
     );
 };
